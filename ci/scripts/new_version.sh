4ff477b3817b60ea2bc44453ec8df7b54e9d6cb5
Joshua Thompson-Lindley <joshua@overflow.digital>
2022-12-13 16:27:45 +0000

    Add auto increment version for CD pipeline

--- ci/scripts/new_version.sh
+++ ci/scripts/new_version.sh
@@ -4,9 +4,23 @@ set -e
 set -x
 
 REPO="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-REPO=$(dirname "$SCRIPTPATH../")
+REPO=$(dirname "$REPO../")
+REPO=$(dirname "$REPO../")
 
-VERSION=$1
+function auto_increment() {
+    VERSION=$(python3 $REPO/ci/scripts/increment_version.py)
+    sed 's/__version__ = .*/__version__ = "'$VERSION'"/g' -i $REPO/flask_openapi/__init__.py
+}
 
-echo 'Bumping version to $VERSION'
-sed 's/__version__ =  .*/__version__ = "'$VERSION'"/g' -i flask_openapi/__init__.py
+function increment_manually() {
+    VERSION=$1
+    sed 's/__version__ = .*/__version__ = "'$VERSION'"/g' -i $REPO/flask_openapi/__init__.py
+}
+
+
+if [ $1 = 'auto_increment' ]; then
+    auto_increment
+else
+    increment_manually
+fi
+
--- ci/scripts/test_runner.sh
+++ ci/scripts/test_runner.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 REPO="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-REPO=$(dirname "$SCRIPTPATH../")
+REPO=$(dirname "$REPO../")
+REPO=$(dirname "$REPO../")
 
 python3 -m pytest $REPO/ci/tests -s -vv --cov --cov-config=$REPO/ci/config/.coveragerc --doctest-modules flask_openapi
