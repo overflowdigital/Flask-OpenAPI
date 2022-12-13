bd4ce5195b991c99a6581c6be1b515f1a47bc1a8
Joshua Thompson-Lindley <joshuathompson@cadosecurity.com>
2022-12-13 11:28:45 +0000

    dont fail on mypy for now

--- ci/scripts/setup_worker.sh
+++ ci/scripts/setup_worker.sh
@@ -6,7 +6,7 @@ set -x
 REPO="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 REPO=$(dirname "$SCRIPTPATH../")
 
-sudo apt update -qq && sudo apt upgrade -y -qq
+sudo apt-get update && sudo apt-get upgrade -y
 python3 -m pip install --upgrade pip
 python3 -m pip install -r ci/requirements.txt
 python3 -m pip install -r requirements.txt
