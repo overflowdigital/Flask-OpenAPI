9049d586ceb6138cafc3c9137cccc0d94f657df5
Joshua Thompson-Lindley <joshuathompson@cadosecurity.com>
2022-12-13 10:18:23 +0000

    More ci change

--- ci/codescan.sh
+++ ci/codescan.sh
@@ -6,7 +6,7 @@ set -x
 REPO="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 REPO=$(dirname "$SCRIPTPATH../")
 
-flake8 $REPO/flask_openapi --extend-ignore=E501
-mypy --show-error-codes --pretty --ignore-missing-imports --disable-error-code attr-defined $REPO/flask_openapi --check-untyped-defs
+flake8 $REPO/flask_openapi --config $REPO/ci/.flake8rc
+mypy --config-file $REPO/ci/.mypyrc $REPO/flask_openapi
 pylint --rcfile=$REPO/ci/.pylintrc $REPO/flask_openapi
 safety check --full-report
