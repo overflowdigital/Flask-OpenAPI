9160721a10de3da76e45e48d20059ecc726bf55e
Joshua Thompson-Lindley <joshua@overflow.digital>
2022-12-13 15:50:43 +0000

    fix test runner

--- ci/scripts/test_runner.sh
+++ ci/scripts/test_runner.sh
@@ -6,4 +6,4 @@ set -x
 REPO="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 REPO=$(dirname "$SCRIPTPATH../")
 
-python3 -m pytest $REPO/ci/tests -s -vv --cov --cov-config=$REPO/.coveragerc --doctest-modules flask_openapi
+python3 -m pytest $REPO/ci/tests -s -vv --cov --cov-config=$REPO/ci/config/.coveragerc --doctest-modules flask_openapi
