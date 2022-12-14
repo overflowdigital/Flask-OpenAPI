327aed0adce72eda504557b7eec9477bc8b34e89
Joshua Thompson-Lindley <joshua@overflow.digital>
2022-12-14 20:47:30 +0000

    whoops

--- ci/scripts/setup_worker.sh
+++ ci/scripts/setup_worker.sh
@@ -11,9 +11,4 @@ sudo apt-get update && sudo apt-get upgrade -y
 python3 -m pip install --upgrade pip
 python3 -m pip install -r $REPO/ci/config/requirements.txt
 python3 -m pip install -r $REPO/requirements.txt
-<<<<<<< HEAD
 sudo python3 -m pip install -e $REPO
-=======
-sudo python3 setup.py develop
-sudo python3 setup.py check
->>>>>>> aaaa1eb1472d50640cb4789ec83391af0aad8de5
