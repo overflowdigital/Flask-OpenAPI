f4f2385f52b8866f35db5759ac92636c640dc984
Joshua Thompson-Lindley <joshua@overflow.digital>
2022-12-14 13:26:40 +0000

    fuck swagger

--- ci/tests/conftest.py
+++ ci/tests/conftest.py
@@ -49,6 +49,7 @@ def get_specs_data(mod):
         response = client.get(url)
         decoded = response.data.decode("utf-8")
         print(decoded)
+        print(spec)
         specs_data[url] = json.loads(decoded)
 
     return specs_data
