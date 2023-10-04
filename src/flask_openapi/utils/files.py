eb89cfe6850ded39bab96dad0ba7d8e0273b8512
Joshua Thompson-Lindley <jthompson@cadosecurity.com>
2023-10-04 13:08:20 +0100

    hmmmm

--- src/flask_openapi/utils/files.py
+++ src/flask_openapi/utils/files.py
@@ -7,12 +7,15 @@ from typing import Literal, Optional
 
 def detect_by_bom(path, default="utf-8"):
     with open(path, "rb") as f:
-        raw = f.read(4)  # will read less if the file is smaller
-    for enc, boms in (
+        raw = f.read(4)
+
+    table = (
         ("utf-8-sig", (codecs.BOM_UTF8,)),
         ("utf-16", (codecs.BOM_UTF16_LE, codecs.BOM_UTF16_BE)),
         ("utf-32", (codecs.BOM_UTF32_LE, codecs.BOM_UTF32_BE)),
-    ):
+    )
+
+    for enc, boms in table:
         if any(raw.startswith(bom) for bom in boms):
             return enc
     return default
