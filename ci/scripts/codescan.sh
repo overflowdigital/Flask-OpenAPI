a77a6dc9508f5b92226b1d5cf8695f6f462beed0
Joshua Thompson-Lindley <jthompson@cadosecurity.com>
2023-10-04 13:56:27 +0100

    dont do diff files then :(

--- ci/scripts/codescan.sh
+++ ci/scripts/codescan.sh
@@ -5,28 +5,22 @@ set -x
 
 TOOL=$1
 
-
 REPO="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 REPO=$(dirname "$REPO../")
 REPO=$(dirname "$REPO../")
 
-diff_files=$(gh pr view $PR --json files --jq '.files | map(select(.path | endswith(".py"))) | .[].path' | sort -u)
-
-
-echo "Running against files: $diff_files"
-
 case $TOOL in
     'black')
-        black --check $diff_files
+        black --check $REPO/src
         ;;
     'isort')
-        isort --check-only $diff_files
+        isort --check-only $REPO/src
         ;;
     'flake8')
-        flake8 $REPO/src/flask_openapi --config $REPO/ci/config/.flake8rc $diff_files
+        flake8 $REPO/src/flask_openapi --config $REPO/ci/config/.flake8rc $REPO/src
         ;;
     'mypy')
-        mypy --config-file $REPO/ci/config/.mypyrc $REPO/src/flask_openapi $diff_files
+        mypy --config-file $REPO/ci/config/.mypyrc $REPO/src/flask_openapi $REPO/src
         ;;
     'safety')
         safety check --full-report --file $REPO/requirements.txt
