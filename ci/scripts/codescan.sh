4e4f56ffdf24d6de9a052b21c557f9a812413fab
Joshua Thompson-Lindley <jthompson@cadosecurity.com>
2023-10-04 14:38:20 +0100

    fix

--- ci/scripts/codescan.sh
+++ ci/scripts/codescan.sh
@@ -17,7 +17,7 @@ case $TOOL in
         flake8 $REPO/src/flask_openapi --config $REPO/ci/config/.flake8rc $REPO/src
         ;;
     'mypy')
-        mypy --config-file $REPO/ci/config/.mypyrc $REPO/src/flask_openapi $REPO/src || true
+        mypy --config-file $REPO/ci/config/.mypyrc $REPO/src || true
         ;;
     'safety')
         safety check --full-report --file $REPO/requirements.txt
